1aa93e2702d648896246bd1ae6a1c606bd2e63ac
Bharat Kalluri <bharatkalluri@protonmail.com>
2018-10-01 02:26:01 +0530

    Configured next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 module.exports = {
+  exportPathMap: async function (defaultPathMap) {
+    return {
+      '/': { page: '/' },
+      '/install': { page: '/install' },
+      '/repo': { page: '/repo' },
+    }
+  },
   webpack: config => {
     // Fixes npm packages that depend on `fs` module
     config.node = {
